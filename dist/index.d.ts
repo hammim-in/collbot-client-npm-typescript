53a0f76920999e30c7b37160b14d30d17b451e3f
Ham Mim <hammim.in@Mac.lan>
2025-03-12 17:29:46 +0530

    viak

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,16 +1,13 @@
-import { JSX } from 'react';
+import * as react_jsx_runtime from 'react/jsx-runtime';
 
 declare global {
     interface Window {
         grecaptcha: any;
     }
 }
-interface ChatProps {
-    theme?: {
-        bgColor?: string;
-        primaryColor?: string;
-    };
-}
-declare const CollBot: ({ theme }: ChatProps) => JSX.Element;
+declare const CollBot: ({ AICHART_RECAPTCHA_FRONTED_KEY, YOUR_SITE_KEY }: {
+    AICHART_RECAPTCHA_FRONTED_KEY?: string;
+    YOUR_SITE_KEY?: string;
+}) => react_jsx_runtime.JSX.Element;
 
 export { CollBot as default };
