266260382ef55084a080b63b61a5067a58f22243
Ham Mim <hammim.in@Mac.lan>
2025-03-14 22:07:11 +0530

    url fixed

--- src/utils/Constents.ts
+++ src/utils/Constents.ts
@@ -1,5 +1,5 @@
 // ENDPOINT
-export const API_ROOT = "https://collbot.hammim.in/"
+export const API_ROOT = "https://collbot.hammim.in"
 export const RECAPTCHA_ROOT = "https://www.google.com/recaptcha/api.js?render="
 
 // API_KEYS
